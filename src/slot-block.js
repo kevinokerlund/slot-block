fb5f3c408f9ef4967ef00d30bda0daabb314e2ad
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-12-30 17:59:36 -0700

    Supports `allow` attribute, and `SlotBlock.allow()` method for specifying rules

---
+++ src/slot-block.js
@@ -0,0 +1,78 @@
+import './polyfills/Element-Matches';
+
+let Instances = new WeakMap();
+
+class SlotBlock {
+	constructor(slot) {
+		this.slot = slot;
+		this.allow = ''; // Might look at making this work inside a function too
+		this.callback = Function.prototype; // noop
+	}
+
+
+	clean() {
+		let nodes = this.slot.assignedNodes();
+		nodes.forEach(node => {
+			let matches = (this.allow.length) ? node.matches(this.allow) : false;
+			let test = this.callback(node);
+
+			if (!matches && !test) {
+				node.removeAttribute('slot');
+			}
+		});
+	}
+
+
+	setEvents() {
+		this.slot.addEventListener('slotchange', (e) => {
+			this.clean();
+		});
+	}
+}
+
+// @Todo Feels like a LOT of logic for allowing two ways of specifying "allow" rules (allow, watch)
+export default {
+	allow(slot, check) {
+		let slotName = slot.getAttribute('name');
+
+		if (!slotName) {
+			return;
+		}
+
+		let instance = Instances.get(slot);
+
+		if (!instance) {
+			instance = new SlotBlock(slot);
+			Instances.set(slot, instance);
+		}
+
+		instance.callback = check;
+	},
+
+	watch(shadowRoot) {
+		let slots = Array.from(shadowRoot.querySelectorAll('slot[name]'));
+		slots.forEach(slot => {
+			let instance = Instances.get(slot);
+			let allowValue = slot.getAttribute('allow');
+			let slotName = slot.getAttribute('name');
+
+			if (!slotName) {
+				return;
+			}
+
+			if (allowValue && !instance) {
+				instance = new SlotBlock(slot);
+				Instances.set(slot, instance);
+				instance.allow = allowValue;
+			}
+			else if (allowValue && instance) {
+				instance.allow = allowValue;
+			}
+
+			if (instance) {
+				instance.clean();
+				instance.setEvents();
+			}
+		});
+	}
+}
